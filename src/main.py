673d6ef4f95d3fc79824644c9c9ab6c52b964d5d
bobokuos <bobokuos@gmail.com>
2025-09-25 19:15:26 +0000

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,13 +2,11 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+return {"message": "Hello World!"}
 
-
-if __name__ == "__main__":
-    import uvicorn
+if **name** == "**main**":
+import uvicorn
 
     uvicorn.run(app, host="0.0.0.0", port=8000)
